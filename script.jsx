cdb6cb9d05d7a9455e7971d4d1ea03014f001293
marcus <marcusteo@hotmail.com>
2019-08-29 14:10:16 +0800

    done basic, input validation, removal of tasks and showing of validation error to user

--- script.jsx
+++ script.jsx
@@ -4,16 +4,31 @@ class List extends React.Component {
 
     this.state = {
       word:"",
-      list : []
+      list : [],
+      alert: ""
+    }
+    this.changeHandler = this.changeHandler.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+
+  addItem(item){
+    if (item.length > 0){
+      let list = this.state.list
+      list.push(item)
+      this.setState({list:list, alert:""})
+    }else{
+      this.setState({alert:"You need to enter something"})
     }
   }
 
-  addItem(){
-    debugger;
+  changeHandler(e){
+    this.setState({word:e.target.value})
   }
 
-  changeHandler(){
-    debugger;
+  removeItem(e){
+    let list = this.state.list
+    list.splice(e.target.value,1)
+    this.setState({list:list})
   }
 
   render() {
@@ -22,15 +37,36 @@ class List extends React.Component {
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
-          <button onClick={()=>{this.addItem()}}>add item</button>
+          <input onChange={(event)=>{this.changeHandler(event)}} value={this.state.word}/>
+          <button onClick={()=>{this.addItem(this.state.word)}}>add item</button>
+          <br/>{this.state.alert}
+          <div>
+          <ul>
+          {this.state.list.map((obj,index)=>{
+            return <Item task={obj} index={index} key={index} remove={this.removeItem.bind(this)}/>
+          })}
+          </ul>
+          </div>
         </div>
+
       );
   }
 }
+
+class Item extends React.Component{
+
+  render(){
+    return(
+      <li>{this.props.task}
+      <button onClick={this.props.remove} value={this.props.index}>Remove task</button>
+      </li>
+    )
+  }
+}
+
+
 
 ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
